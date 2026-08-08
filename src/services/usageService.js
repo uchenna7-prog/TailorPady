af5ecbe64e9e7435b104c71c3e4a684b35a73f86
uchendu uchenna Daniel <uchenduuchenna7@gmail.com>
2026-08-08 17:58:01 +0100

    Refactor USAGE_LIMITS export in usageService.js

--- src/services/usageService.js
+++ src/services/usageService.js
@@ -1,15 +1,7 @@
 import { doc, onSnapshot, setDoc, increment } from 'firebase/firestore'
+import { USAGE_LIMITS } from '../datas/usageLimits'
 
-export const USAGE_LIMITS = {
-  customers: 8,
-  measurementsPerMonth: 8,
-  ordersPerMonth: 10,
-  invoicesPerMonth: 5,
-  receiptsPerMonth: 5,
-  portfolioUploadsPerMonth: 5,
-  reviewLinksPerMonth: 8,
-  aiActionsPerMonth: 3,
-}
+export { USAGE_LIMITS }
 
 function getCurrentMonthKey() {
   const now = new Date()
