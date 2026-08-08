14e846f71e6361f937bbdd5259d9d95df349f37f
uchendu uchenna Daniel <uchenduuchenna7@gmail.com>
2026-08-08 17:56:51 +0100

    Add usageLimits.js with defined usage limits

---
+++ src/datas/usageLimits.js
@@ -0,0 +1,10 @@
+export const USAGE_LIMITS = {
+  customers: 8,
+  measurementsPerMonth: 8,
+  ordersPerMonth: 10,
+  invoicesPerMonth: 5,
+  receiptsPerMonth: 5,
+  portfolioUploadsPerMonth: 5,
+  reviewLinksPerMonth: 8,
+  aiActionsPerMonth: 3,
+}
