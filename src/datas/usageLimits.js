edc1650cc16acb89935419bb938356893da657a8
uchendu uchenna Daniel <uchenduuchenna7@gmail.com>
2026-08-11 08:04:43 +0100

    Update usage limits in usageLimits.js
    Reduced usage limits for customers, measurements, orders, and review links.

--- src/datas/usageLimits.js
+++ src/datas/usageLimits.js
@@ -1,10 +1,11 @@
 export const USAGE_LIMITS = {
-  customers: 8,
-  measurementsPerMonth: 8,
-  ordersPerMonth: 10,
+  customers: 5,
+  measurementsPerMonth: 5,
+  ordersPerMonth: 5,
   invoicesPerMonth: 5,
   receiptsPerMonth: 5,
   portfolioUploadsPerMonth: 5,
-  reviewLinksPerMonth: 8,
+  reviewLinksPerMonth: 5,
+  paymentRecordsPerMonth: 5,
   aiActionsPerMonth: 3,
 }
