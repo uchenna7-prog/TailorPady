4cb87c374f92631e6d186f5cb11523ddf8fb7a55
uchendu uchenna Daniel <uchenduuchenna7@gmail.com>
2026-08-17 08:50:41 +0100

    Create ReferralCelebrationFlow/ReferralCelebrationFlow.jsx

---
+++ src/components/ReferralCelebrationFlow/ReferralCelebrationFlow.jsx
@@ -0,0 +1,37 @@
+import { useState, useEffect } from 'react'
+import { useReferral } from '../../contexts/ReferralContext'
+import { usePremium } from '../../contexts/PremiumContext'
+import { usePremiumSuccess } from '../../contexts/PremiumSuccessContext'
+import ReferralRewardModal from '../ReferralRewardModal/ReferralRewardModal'
+
+export default function ReferralCelebrationFlow() {
+  const { pendingReferralReward, acknowledgeReferralReward } = useReferral()
+  const { billingCycle, nextRenewal } = usePremium()
+  const { triggerPremiumSuccess } = usePremiumSuccess()
+
+  const [activeReward, setActiveReward] = useState(null)
+  const [rewardModalOpen, setRewardModalOpen] = useState(false)
+
+  useEffect(() => {
+    if (pendingReferralReward && !activeReward) {
+      setActiveReward(pendingReferralReward)
+      setRewardModalOpen(true)
+    }
+  }, [pendingReferralReward, activeReward])
+
+  const handleRewardContinue = () => {
+    const reward = activeReward
+    setRewardModalOpen(false)
+    acknowledgeReferralReward(reward.id)
+    triggerPremiumSuccess({ billingCycle, nextRenewal }, () => setActiveReward(null))
+  }
+
+  if (!rewardModalOpen || !activeReward) return null
+
+  return (
+    <ReferralRewardModal
+      referredName={activeReward.referredDisplayName}
+      onContinue={handleRewardContinue}
+    />
+  )
+}
