9b7dc3bebac13cf5527628e65e71ef3289485819
uchendu uchenna Daniel <uchenduuchenna7@gmail.com>
2026-08-29 07:13:01 +0100

    Update logo image source in Welcome component

--- src/components/FirstRunFlow/Welcome.jsx
+++ src/components/FirstRunFlow/Welcome.jsx
@@ -5,7 +5,7 @@ export default function Welcome({ onDone }) {
     <div className={styles.page}>
       <div className={styles.welcomeContent}>
         <div className={styles.brandMark}>
-          <img src="/icons/icon192.png" alt="TailorPady" />
+          <img src="/icons/icon512.png" alt="TailorPady" />
         </div>
         <h1 className={styles.wordmark}>TailorPady</h1>
       </div>
